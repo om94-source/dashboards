7bcd326d14465fb57401b4c715181a999bcf84c9
om94-source <omaralaridi1994@gmail.com>
2024-07-17 17:29:33 +0200

    Weather dashboard ready

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,53 +9,43 @@ st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 with open("styles.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+
 # Sidebar
-st.sidebar.header("Seattle Weather Dashboard")
-st.sidebar.subheader("Heatmap parameter")
-heatmap_chart_color = st.sidebar.selectbox("Color by", ["temp_min", "temp_max"])
+st.sidebar.header("Weather Dashboard")
+page = st.sidebar.selectbox("Select a page", ["Heatmap", "Line Chart"])
 
-st.sidebar.subheader("Donut chart parameter")
-donnut_chart_theta = st.sidebar.selectbox("Select data", ["q2", "q3"])
+if page == "Heatmap":
+    st.sidebar.subheader("Heatmap parameter")
+    heatmap_chart_color = st.sidebar.selectbox("Color by", ["precipitation", "temp_min", "temp_max", "wind"])
 
-st.sidebar.subheader("Line chart parameters")
-plot_data = st.sidebar.multiselect('Select data', ["temp_min", "temp_max"], ["temp_min", "temp_max"])
-plot_height = st.sidebar.slider('Specify line plot height', 200, 500, 250)
-
-# st.sidebar.subheader("Bar chart parameter")
-# bar_chart = st.sidebar.selectbox("Filter by", ["1", "2"])
+if page == "Line Chart":
+    st.sidebar.subheader("Line chart parameters")
+    plot_data = st.sidebar.multiselect('Select data', ["precipitation", "temp_min", "temp_max", "wind"], ["temp_min", "temp_max"])
+    plot_height = st.sidebar.slider('Specify line plot height', 200, 800, 400)
 
 st.sidebar.markdown('''
                     ---
                     **Created by** [Omar Alaridi](https://www.linkedin.com/in/omar-alaridi)
                     ''')
 
-# Main page
-# Row B
+# Load data
 seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
 stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
 
-c1, c2 = st.columns((7,3))
-with c1:
+# Main page content
+if page == "Heatmap":
     st.markdown('### Heatmap')
     plost.time_hist(
-    data=seattle_weather,
-    date='date',
-    x_unit='week',
-    y_unit='day',
-    color=heatmap_chart_color,
-    aggregate='median',
-    legend=None,
-    height=345,
-    use_container_width=True)
-with c2:
-    st.markdown('### Donut chart')
-    plost.donut_chart(
-        data=stocks,
-        theta=donnut_chart_theta,
-        color='company',
-        legend='bottom', 
+        data=seattle_weather,
+        date='date',
+        x_unit='week',
+        y_unit='day',
+        color=heatmap_chart_color,
+        aggregate='median',
+        legend=None,
+        height=345,
         use_container_width=True)
 
-# Row C
-st.markdown('### Line chart')
-st.line_chart(seattle_weather, x = 'date', y = plot_data, height = plot_height)
+elif page == "Line Chart":
+    st.markdown('### Line chart')
+    st.line_chart(seattle_weather, x='date', y=plot_data, height=plot_height)
